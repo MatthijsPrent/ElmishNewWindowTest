f1a1d81f499ebcce2297ceb10c422a370deeb7ce
Matthijs Prent <matthijs.prent@Applicos.com>
2022-10-28 13:09:44 +0200

    Add project files.

---
+++ ElmishNewWindowTest.Gui.Views/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ElmishNewWindowTest.Gui.Views
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.Activated += StartElmish;
+        }
+
+        private void StartElmish (object sender, EventArgs e)
+        {
+            this.Activated -= StartElmish;
+            ElmishNewWindowTest.Gui.mainModule.main(MainWindow, () => new Window1());
+        }
+
+    }
+}
---
+++ ElmishNewWindowTest.Gui/Library.fs
@@ -0,0 +1,54 @@
+﻿namespace ElmishNewWindowTest.Gui
+open System
+open System.Windows
+open Elmish
+open Elmish.WPF
+
+module mainModule =
+
+    type Model = 
+        {
+            window: WindowState<string>
+        }
+
+    let init () = 
+        {
+            window = WindowState.Closed
+        }, Cmd.none
+    type mainmsg =
+        | OpenWindow
+        | CloseWindow
+    
+    let update msg m =
+        match msg with
+        | OpenWindow ->
+            {m with window = m.window |> WindowState.toVisible ""} , 
+            Cmd.none
+        | CloseWindow ->
+            {m with window = Elmish.WPF.WindowState.Closed}, 
+            Cmd.none
+
+    let subwindowBindings () =
+        [
+             "CloseInstrumentSetupDialog" 
+             |>  Binding.cmd (CloseWindow)
+        ]
+    let bindings (createsetupdialog: unit -> #Window)() =
+        [
+            "Open" |> Binding.cmd OpenWindow
+            "Close" |> Binding.cmd CloseWindow
+            "subwindow" |>Binding.subModelWin(
+                (fun m -> m.window),
+                (subwindowBindings),
+                createsetupdialog
+                )
+            ]
+
+    let main window (createExtraWindow: Func<#Window>)= 
+        let createDialog () =
+            let window = createExtraWindow.Invoke ()
+            window
+        let newBindings = bindings createDialog
+
+        WpfProgram.mkProgram init update newBindings
+        |> WpfProgram.startElmishLoop window
